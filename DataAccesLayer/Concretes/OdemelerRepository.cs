18cc9e0b150b4320b1e7b8d1e4c03c9fcfba2b65
irem <i_remunver@hotmail.com>
2019-04-23 22:02:33 +0300

    repository'e fonksiyon yazıldı

--- DataAccesLayer/Concretes/OdemelerRepository.cs
+++ DataAccesLayer/Concretes/OdemelerRepository.cs
@@ -91,23 +91,292 @@ namespace DataAccesLayer.Concretes
 
         public bool Guncelle(Odemeler entity)
         {
-            throw new NotImplementedException();
+            _rowsAffected = 0;
+            _errorCode = 0;
+
+            try
+            {
+                var query = new StringBuilder();
+                query.Append(" UPDATE [dbo].[tblOdeme] ");
+                query.Append(" SET [OdemeID] = @OdemeID, [OdemeMiktari] = @OdemeMiktari, [OdemeBasarili] =  @OdemeBasarili, [OdemeTarihi] = @OdemeTarihi ");
+                query.Append(" WHERE ");
+                query.Append(" [OdemeID] = @OdemeID ");
+                query.Append(" SELECT @intErrorCode = @@ERROR; ");
+
+                var commandText = query.ToString();
+                query.Clear();
+
+                using (var dbConnection = _dbProviderFactory.CreateConnection())
+                {
+                    if (dbConnection == null)
+                        throw new ArgumentNullException("dbConnection", "The db connection can't be null.");
+
+                    dbConnection.ConnectionString = _connectionString;
+
+                    using (var dbCommand = _dbProviderFactory.CreateCommand())
+                    {
+                        if (dbCommand == null)
+                            throw new ArgumentNullException("dbCommand" + " The db Insert command for entity [tbl_Customers] can't be null. ");
+
+                        dbCommand.Connection = dbConnection;
+                        dbCommand.CommandText = commandText;
+
+                        //Input Params
+                        DBHelper.AddParameter(dbCommand, "@OdemeID", entity.OdemeID);
+                        DBHelper.AddParameter(dbCommand, "@OdemeMiktari",  entity.OdemeMiktari);
+                        DBHelper.AddParameter(dbCommand, "@OdemeBasarili", entity.OdemeBasarili);
+                        DBHelper.AddParameter(dbCommand, "@OdemeTarihi", entity.OdemeTarihi);
+
+                        //Output Params
+                        DBHelper.AddParameter(dbCommand, "@intErrorCode", null);
+
+                        //Open Connection
+                        if (dbConnection.State != ConnectionState.Open)
+                            dbConnection.Open();
+
+                        //Execute query
+                        _rowsAffected = dbCommand.ExecuteNonQuery();
+                        _errorCode = int.Parse(dbCommand.Parameters["@intErrorCode"].Value.ToString());
+
+                        if (_errorCode != 0)
+                            throw new Exception("Updating Error for entity [tblOdeme] reported the Database ErrorCode: " + _errorCode);
+                    }
+                }
+                //Return the results of query/ies
+                return true;
+            }
+            catch (Exception ex)
+            {
+              
+                throw new Exception("CustomersRepository::Update:Error occured.", ex);
+            }
         }
+    
 
         public IList<Odemeler> HepsiniSec()
         {
-            throw new NotImplementedException();
+            _errorCode = 0;
+            _rowsAffected = 0;
+
+            IList<Odemeler> odemeler = new List<Odemeler>();
+
+            try
+            {
+                var query = new StringBuilder();
+                query.Append("SELECT ");
+                query.Append(
+                    "[OdemeID], [OdemeMiktari], [OdemeBasarili], [OdemeTarihi]");
+                query.Append("FROM [dbo].[tblOdeme] ");
+                query.Append("SELECT @intErrorCode=@@ERROR; ");
+
+                var commandText = query.ToString();
+                query.Clear();
+
+                using (var dbConnection = _dbProviderFactory.CreateConnection())
+                {
+                    if (dbConnection == null)
+                        throw new ArgumentNullException("dbConnection", "The db connection can't be null.");
+
+                    dbConnection.ConnectionString = _connectionString;
+
+                    using (var dbCommand = _dbProviderFactory.CreateCommand())
+                    {
+                        if (dbCommand == null)
+                            throw new ArgumentNullException(
+                                "dbCommand" + " The db SelectById command for entity [tblOdeme] can't be null. ");
+
+                        dbCommand.Connection = dbConnection;
+                        dbCommand.CommandText = commandText;
+
+                        //Input Parameters - None
+
+                        //Output Parameters
+                        DBHelper.AddParameter(dbCommand, "@intErrorCode", null);
+
+                        //Open Connection
+                        if (dbConnection.State != ConnectionState.Open)
+                            dbConnection.Open();
+
+                        //Execute query.
+                        using (var reader = dbCommand.ExecuteReader())
+                        {
+                            if (reader.HasRows)
+                            {
+                                while (reader.Read())
+                                {
+                                    var entity = new Odemeler();
+                                    entity.OdemeID = reader.GetInt32(0);
+                                    entity.OdemeMiktari = reader.GetDecimal(1);
+                                    entity.OdemeBasarili = reader.GetBoolean(2);
+                                    entity.OdemeTarihi = reader.GetDateTime(3);
+                                    odemeler.Add(entity);
+                                }
+                            }
+
+                        }
+
+                        _errorCode = int.Parse(dbCommand.Parameters["@intErrorCode"].Value.ToString());
+
+                        if (_errorCode != 0)
+                        {
+                            // Throw error.
+                            throw new Exception("Selecting All Error for entity [tblOdeme] reported the Database ErrorCode: " + _errorCode);
+
+                        }
+                    }
+                }
+                // Return list
+                return odemeler;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("CustomersRepository::SelectAll:Error occured.", ex);
+            }
         }
 
         public Odemeler IdSec(int id)
         {
-            throw new NotImplementedException();
+            _errorCode = 0;
+            _rowsAffected = 0;
+
+            Odemeler odemeler = null;
+
+            try
+            {
+                var query = new StringBuilder();
+                query.Append("SELECT ");
+                query.Append(
+                    "[OdemeID], [OdemeMiktari], [OdemeBasarili], [OdemeTarihi] ");
+                query.Append("FROM [dbo].[tblOdeme] ");
+                query.Append("WHERE ");
+                query.Append("[OdemeID] = @id ");
+                query.Append("SELECT @intErrorCode=@@ERROR; ");
+
+                var commandText = query.ToString();
+                query.Clear();
+
+                using (var dbConnection = _dbProviderFactory.CreateConnection())
+                {
+                    if (dbConnection == null)
+                        throw new ArgumentNullException("dbConnection", "The db connection can't be null.");
+
+                    dbConnection.ConnectionString = _connectionString;
+
+                    using (var dbCommand = _dbProviderFactory.CreateCommand())
+                    {
+                        if (dbCommand == null)
+                            throw new ArgumentNullException(
+                                "dbCommand" + " The db SelectById command for entity [tblOdeme] can't be null. ");
+
+                        dbCommand.Connection = dbConnection;
+                        dbCommand.CommandText = commandText;
+
+                        //Input Parameters
+                        DBHelper.AddParameter(dbCommand, "@id", id);
+
+                        //Output Parameters
+                        DBHelper.AddParameter(dbCommand, "@intErrorCode", null);
+
+                        //Open Connection
+                        if (dbConnection.State != ConnectionState.Open)
+                            dbConnection.Open();
+
+                        //Execute query.
+                        using (var reader = dbCommand.ExecuteReader())
+                        {
+                            if (reader.HasRows)
+                            {
+                                while (reader.Read())
+                                {
+                                    var entity = new Odemeler();
+                                    entity.OdemeID = reader.GetInt32(0);
+                                    entity.OdemeMiktari = reader.GetDecimal(1);
+                                    entity.OdemeBasarili = reader.GetBoolean(2);
+                                    entity.OdemeTarihi = reader.GetDateTime(3);
+                                    odemeler = entity;
+                                    break;
+                                }
+                            }
+                        }
+
+                        _errorCode = int.Parse(dbCommand.Parameters["@intErrorCode"].Value.ToString());
+
+                        if (_errorCode != 0)
+                        {
+                            // Throw error.
+                            throw new Exception("Selecting Error for entity [tblOdeme] reported the Database ErrorCode: " + _errorCode);
+                        }
+                    }
+                }
+
+                return odemeler;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("CustomersRepository::SelectById:Error occured.", ex);
+            }
         }
 
 
         public bool IdSil(int id)
         {
-            throw new NotImplementedException();
+            _errorCode = 0;
+            _rowsAffected = 0;
+
+            try
+            {
+                var query = new StringBuilder();
+                query.Append("DELETE ");
+                query.Append("FROM [dbo].[tblOdeme] ");
+                query.Append("WHERE ");
+                query.Append("[tblOdeme] = @OdemeID ");
+                query.Append("SELECT @intErrorCode=@@ERROR; ");
+
+                var commandText = query.ToString();
+                query.Clear();
+
+                using (var dbConnection = _dbProviderFactory.CreateConnection())
+                {
+                    if (dbConnection == null)
+                        throw new ArgumentNullException("dbConnection", "The db connection can't be null.");
+
+                    dbConnection.ConnectionString = _connectionString;
+
+                    using (var dbCommand = _dbProviderFactory.CreateCommand())
+                    {
+                        if (dbCommand == null)
+                            throw new ArgumentNullException(
+                                "dbCommand" + " The db SelectById command for entity [tblOdeme] can't be null. ");
+
+                        dbCommand.Connection = dbConnection;
+                        dbCommand.CommandText = commandText;
+
+                        //Input Parameters
+                        DBHelper.AddParameter(dbCommand, "@id", id);
+
+                        //Output Parameters
+                        DBHelper.AddParameter(dbCommand, "@intErrorCode", null);
+
+                        //Open Connection
+                        if (dbConnection.State != ConnectionState.Open)
+                            dbConnection.Open();
+                        //Execute query
+                        _rowsAffected = dbCommand.ExecuteNonQuery();
+                        _errorCode = int.Parse(dbCommand.Parameters["@intErrorCode"].Value.ToString());
+
+                        if (_errorCode != 0)
+                            throw new Exception(
+                                "Deleting Error for entity [tblOdeme] reported the Database ErrorCode: " +
+                                _errorCode);
+                    }
+                }
+                //Return the results of query/ies
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("TransactionsRepository::Insert:Error occured.", ex);
+            }
         }
     }
 }
