b8df4ec334d40f288612a209443b9dff0495eb1c
Birkan <birkankader@outlook.com>
2019-05-18 01:53:23 +0300

    Revert "Kullanıcı Detay, Listele ve Düzenle tamamlandı. Kullanıcı valid eklendi."
    This reverts commit 64882fa26337f6496c81ffbc789d0122a462e4f3.

--- Models/Concretes/Kullanici.cs
+++ Models/Concretes/Kullanici.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,42 +9,15 @@ namespace Models.Concretes
     public class Kullanici
     {
         public int KullaniciID { get; set; }
-
-        [Required(ErrorMessage = "Kimlik numarası girmelisiniz.")]
-        [StringLength(11, MinimumLength = 11)]
         public string TcKimlik { get; set; }
-
-        [Required(ErrorMessage = "İsim girmelisiniz.")]
-        [StringLength(50, MinimumLength = 3)]
         public string Ad { get; set; }
-
-        [Required(ErrorMessage = "Soyisim girmelisiniz.")]
-        [StringLength(50, MinimumLength = 3)]
         public string Soyad { get; set; }
-
-        [Required(ErrorMessage = "Doğum tarihi girmelisiniz.")]
         public DateTime DogumTarihi { get; set; }
-
-        [Required(ErrorMessage = "Adres girmelisiniz.")]
-        
         public string Adres { get; set; }
-
-        [Required(ErrorMessage = "Telefon girmelisiniz.")]
-        [StringLength(15, MinimumLength = 11)]
         public string Telefon { get; set; }
-
-        [Required(ErrorMessage = "Email girmelisiniz.")]
-        [StringLength(50, MinimumLength = 3)]
         public string Email { get; set; }
-
-        [Required(ErrorMessage = "Kullanıcı adı girmelisiniz.")]
-        [StringLength(50, MinimumLength = 3)]
         public string KullaniciAdi { get; set; }
-
-        [Required(ErrorMessage = "Parola girmelisiniz.")]
-        [StringLength(50, MinimumLength = 6)]
         public string Parola { get; set; }
-
         public bool Durum { get; set; }
         public string KullaniciTipi { get; set; }
         public Guid Anahtar { get; set; }
